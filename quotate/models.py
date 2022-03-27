597bc1da972e976b771eca9e092428656df551f7
Sean Tolbert <seantolbert@Seans-MacBook-Pro.local>
2022-03-27 19:44:28 -0400

    stubs up 404 css page

---
+++ quotate/models.py
@@ -0,0 +1,18 @@
+from os import link
+from django.db import models
+from django.urls import reverse
+
+class Source(models.Model):
+    first_name = models.CharField(max_length=10)
+    last_name = models.CharField(max_length=10)
+    history = models.TextField(required=False)
+    hyperlink = models.URLField()
+
+    def __str__(self):
+        name = f"{self.first_name} {self.last_name}"
+        return name
+class Quote(models.Model):
+    text = models.TextField(max_length=1000)
+    source = models.ManyToManyField(Source)
+    
+    
